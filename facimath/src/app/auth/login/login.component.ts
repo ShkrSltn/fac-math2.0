b822d62404a4548cc20dbd140e9a012c3811c4b0
ShkrSltn <shakir.sultanov@powercoders.org>
2024-07-02 23:47:53 +0200

    login incorrect notify added

--- facimath/src/app/auth/login/login.component.ts
+++ facimath/src/app/auth/login/login.component.ts
@@ -14,6 +14,7 @@ import { Router, RouterModule } from '@angular/router';
 })
 export class LoginComponent {
   loginData = { login: '', password: '' };
+  loginError: string | null = null;
 
   constructor(private authService: AuthService, private router: Router) {}
 
@@ -31,7 +32,8 @@ export class LoginComponent {
             }
           },
           (error) => {
-            console.log('Ошибка авторизации', error);
+            console.log('Authorization error', error);
+            this.loginError = 'Incorrect login or password';
           }
         );
     }
